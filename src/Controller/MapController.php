327927d1b6b38367aaf048a361505c43a022015d
malik <shahzaib94400@gmail.com>
2024-10-02 15:39:52 +0200

    MapController fix

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,21 +2,17 @@
 
 namespace App\Controller;
 
-use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MapController extends AbstractController
 {
     #[Route('/map', name: 'app_map')]
-
     public function execute(): Response
     {
-
+        // Premier appel cURL pour les stations
         $curl = curl_init();
-
-
         curl_setopt_array($curl, [
             CURLOPT_PORT => "9042",
             CURLOPT_URL => "http://localhost:9042/api/stations",
@@ -25,23 +21,21 @@ class MapController extends AbstractController
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_POSTFIELDS => "",
             CURLOPT_SSL_VERIFYPEER => false
         ]);
 
         $response = curl_exec($curl);
-
         $err = curl_error($curl);
-
         curl_close($curl);
 
-
         if ($err) {
-            echo "cURL Error #:" . $err;
+            echo "Erreur cURL : " . $err;
+            $response = false; // Gérer l'erreur d'une autre manière si nécessaire
         } else {
             $response = json_decode($response, true);
         }
 
+        // Deuxième appel cURL pour le statut des vélos
         $curl2 = curl_init();
         curl_setopt_array($curl2, [
             CURLOPT_PORT => "9042",
@@ -51,48 +45,52 @@ class MapController extends AbstractController
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_POSTFIELDS => "",
             CURLOPT_SSL_VERIFYPEER => false
         ]);
 
         $response2 = curl_exec($curl2);
-
         $err2 = curl_error($curl2);
-
         curl_close($curl2);
 
         if ($err2) {
-            echo "cURL Error #:" . $err2;
-        }else{
+            echo "Erreur cURL : " . $err2;
+            $response2 = false; // Gérer l'erreur d'une autre manière si nécessaire
+        } else {
             $response2 = json_decode($response2, true);
         }
 
+        // Vérification avant de boucler sur les réponses
+        if ($response === false) {
+            echo "Erreur lors de la récupération des données des stations.";
+        } elseif ($response2 === false) {
+            echo "Erreur lors de la récupération des données de statut des vélos.";
+        } else {
+            $stations = [];
 
-        $stations = [];
-
-        foreach ($response as $infostat) {
-            foreach ($response2 as $infovelo) {
-                if ($infostat['station_id'] == $infovelo['station_id']) {
-                    $stations_data = [
-                        'nom' => $infostat['name'],
-                        'lat' => $infostat['lat'],
-                        'lon' => $infostat['lon'],
-                        'velodispo' => $infovelo['num_bikes_available'],
-                        'velomecha' => $infovelo['num_bikes_available_types'][0]['mechanical'],
-                        'velomelec' => $infovelo['num_bikes_available_types'][1]['ebike']
-                    ];
-                    $stations[] = $stations_data; // opérateur d'assignation corrigé pour ajouter au tableau
-                    // var_dump($stations);
-                    break;
-
+            foreach ($response as $infostat) {
+                foreach ($response2 as $infovelo) {
+                    if ($infostat['station_id'] == $infovelo['station_id']) {
+                        $stations_data = [
+                            'nom' => $infostat['name'],
+                            'lat' => $infostat['lat'],
+                            'lon' => $infostat['lon'],
+                            'velodispo' => $infovelo['num_bikes_available'],
+                            'velomecha' => $infovelo['num_bikes_available_types'][0]['mechanical'],
+                            'velomelec' => $infovelo['num_bikes_available_types'][1]['ebike']
+                        ];
+                        $stations[] = $stations_data;
+                        break;
+                    }
                 }
             }
+
+            return $this->render('map/map.html.twig', [
+                "titre"   => 'MapController',
+                "stations" => $stations
+            ]);
         }
 
-        return $this->render('map/map.html.twig', [
-            "titre"   => 'MapController',
-            "stations" => $stations
-
-        ]);
+        // Si une erreur survient, retourner une réponse vide ou d'erreur
+        return new Response("Erreur lors de l'affichage des stations.");
     }
 }
